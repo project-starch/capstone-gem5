aaa8833d0fc67941e2a04e8a207a4eefbfe6ce9d
Jason Yu <yuzhijingcheng1996@hotmail.com>
2022-09-07 14:54:02 +0800

    resolved collision

--- tests/capstone/make-coreutils.sh
+++ tests/capstone/make-coreutils.sh
@@ -1,5 +1,12 @@
+make
+
+if [ ! -d ./coreutils ]; then 
+    wget -q -O /dev/stdout https://ftp.gnu.org/gnu/coreutils/coreutils-9.1.tar.gz | tar xzf /dev/stdin
+    mv coreutils-9.1 coreutils
+fi
+
 cd coreutils
 mkdir -p install
-./configure --prefix=$(pwd)/install --host=riscv64-unknown-linux-gnu LDFLAGS="-static -Wl,--wrap=malloc -Wl,--wrap=free -L$(pwd)/.." LIBS="-lc -lpthread -linterp"
+./configure --prefix=$(pwd)/install --host=riscv64-unknown-linux-gnu LDFLAGS="-static -Wl,-z,muldefs -Wl,--wrap=malloc -Wl,--wrap=free -L$(pwd)/.." LIBS="-lc -lpthread -linterp"
 make -j$(nproc)
 make install
