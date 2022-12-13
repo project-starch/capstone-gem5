0f5d8536675f45372315c93eae7e091a3ac3af93
Aditya Badole <supershinx@gmail.com>
2022-12-13 09:18:53 +0000

    Fix insn macros in header

--- tests/capstone/o3/asm_insn.h
+++ tests/capstone/o3/asm_insn.h
@@ -1,17 +1,17 @@
 #define REVOKE(r)        .insn i 0x77, 0x1, x0, r, 0
 
-#define CALL(r,r)        .insn i 0x77, 0x2, r, r, 0
+#define CALL(rd,rs1)      .insn i 0x77, 0x2, rd, rs1, 0
 #define JMP(r)           .insn i 0x77, 0x6, x0, r, 0
-#define JNZ(r,r)         .insn i 0x77, 0x7, r, r, 0
+#define JNZ(rd,rs1)         .insn i 0x77, 0x7, rd, rs1, 0
 
-#define SHRINK(r,r,r)    .insn r 0x6B, 0x0, 0x0, r, r, r
-#define LT(r,r,r)        .insn r 0x6B, 0x0, 0x1, r, r, r
-#define TIGHTEN(r,r)     .insn i 0x6B, 0x1, r, r, 0
+#define SHRINK(rd,rs1,rs2)    .insn r 0x6B, 0x0, 0x0, rd, rs1, rs2
+#define LT(rd,rs1,rs2)        .insn r 0x6B, 0x0, 0x1, rd, rs1, rs2
+#define TIGHTEN(rd,rs1)     .insn i 0x6B, 0x1, rd, rs1, 0
 #define DELIN(r)         .insn i 0x6B, 0x2, r, x0, 0
-#define SCC(r,r)         .insn i 0x6B, 0x3, r, r, 0
+#define SCC(rd,rs1)         .insn i 0x6B, 0x3, rd, rs1, 0
 #define INIT(r)          .insn i 0x6B, 0x4, r, x0, 0
-#define SEAL(r)          .insn i 0x6B, 0x5, r, x0, 0
-#define ADD2(r,r)        .insn i 0x6B, 0x6, r, r, 0
+#define SEAL(r)           .insn i 0x6B, 0x5, r, x0, 0
+#define ADD2(rd,rs1)        .insn i 0x6B, 0x6, rd, rs1, 0
 
 //Test instructions for node ops
 #define QUERY(r)         .insn i 0x5F, 0x0, x0, r, 0
